7903b842da25f6b1b147198ba8315719b10f00d5
Zohaad Fazal <zohaad.fazal@gmail.com>
2016-08-31 00:57:03 +0200

    site finished

---
+++ myJS.js
@@ -0,0 +1,25 @@
+(function(exports, d) {
+  function domReady(fn, context) {
+
+    function onReady(event) {
+      d.removeEventListener("DOMContentLoaded", onReady);
+      fn.call(context || exports, event);
+    }
+
+    function onReadyIe(event) {
+      if (d.readyState === "complete") {
+        d.detachEvent("onreadystatechange", onReadyIe);
+        fn.call(context || exports, event);
+      }
+    }
+
+    d.addEventListener && d.addEventListener("DOMContentLoaded", onReady) ||
+    d.attachEvent      && d.attachEvent("onreadystatechange", onReadyIe);
+  }
+
+  exports.domReady = domReady;
+})(window, document);
+
+domReady(function(event){
+  document.getElementById('container').classList.remove('loading')
+});
